d2e6e523c835e741f3e3e8d729360db0a6467352
Christopher Gu <christopher.gu@gmail.com>
2014-04-14 11:32:02 -0500

    Initial Commit

---
+++ Foodys/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Foodys
+//
+//  Created by Christopher Gu on 4/14/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoodysTests/FoodysTests.m
@@ -0,0 +1,34 @@
+//
+//  FoodysTests.m
+//  FoodysTests
+//
+//  Created by Christopher Gu on 4/14/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FoodysTests : XCTestCase
+
+@end
+
+@implementation FoodysTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
