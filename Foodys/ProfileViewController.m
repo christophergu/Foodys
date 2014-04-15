144034b7c5c43257c05a33a7b483e93210af037c
Christopher Gu <christopher.gu@gmail.com>
2014-04-14 19:30:42 -0500

    changing avatar photos is possible now

--- Foodys/ProfileViewController.m
+++ Foodys/ProfileViewController.m
@@ -9,8 +9,9 @@
 #import "ProfileViewController.h"
 #import <Parse/Parse.h>
 
-@interface ProfileViewController ()
+@interface ProfileViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (strong, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (strong, nonatomic) IBOutlet UIButton *myAvatarPhotoButton;
 
 @end
 
@@ -19,14 +20,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    self.avatarImageView.image = [UIImage imageNamed:@"defaultUserImage"];
+    if (self.avatarImageView.image == nil) {
+        self.avatarImageView.image = [UIImage imageNamed:@"defaultUserImage"];
+    }
 }
 
 - (IBAction)onLogOutButtonPressed:(id)sender
 {
     [PFUser logOut];
     [self performSegueWithIdentifier:@"LogInSegue" sender:self];
+}
+
+- (IBAction)onImageViewButtonPressed:(id)sender
+{
+    UIImagePickerController * picker = [[UIImagePickerController alloc] init];
+	picker.delegate = self;
+    
+	if((UIButton *) sender == self.myAvatarPhotoButton) {
+		picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+	} else {
+		picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+	}
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+    
+    // saving a uiimage to pffile
+    UIImage *pickedImage = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    NSData* data = UIImageJPEGRepresentation(pickedImage,1.0f);
+    PFFile *imageFile = [PFFile fileWithData:data];
+    
+    // getting a uiimage from pffile
+    [imageFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        if (!error) {
+            UIImage *photo = [UIImage imageWithData:data];
+            self.avatarImageView.image = photo;
+        }
+    }];
 }
 
 @end
