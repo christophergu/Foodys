a2f2df66233f654851c184291e0207e55f21d6c7
Christopher Gu <christopher.gu@gmail.com>
2014-04-16 21:42:50 -0500

    added some basic info to the news detail view controller

--- Foodys/NewsDetailViewController.m
+++ Foodys/NewsDetailViewController.m
@@ -9,6 +9,9 @@
 #import "NewsDetailViewController.h"
 
 @interface NewsDetailViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *dateLabel;
+@property (strong, nonatomic) IBOutlet UITextView *myTextView;
+@property (strong, nonatomic) IBOutlet UITextField *subjectTextField;
 
 @end
 
@@ -17,8 +20,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.myTextView.layer.cornerRadius=8.0f;
+    self.myTextView.layer.masksToBounds=YES;
+    self.myTextView.layer.borderColor=[[[UIColor grayColor] colorWithAlphaComponent:0.2] CGColor];
+    self.myTextView.layer.borderWidth= 1.0f;
     
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"yyyy-MM-dd"];
+    NSDate *postDate = self.currentPost[@"date"];
+    NSString *postString = [dateFormat stringFromDate:postDate];
+    
+    self.dateLabel.text = [NSString stringWithFormat:@"%@",postString];
+    self.subjectTextField.text = self.currentPost[@"title"];
+    self.myTextView.text = self.currentPost[@"body"];
 }
 
 @end
