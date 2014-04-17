cdb9c24ecdd0c4d2ff36065eb9ab1159bcc6ba31
braxm7 <matt.brax@loop.colum.edu>
2014-04-17 12:53:13 -0500

    set up the recommedation view controller

---
+++ Foodys/recommendViewController.h
@@ -0,0 +1,13 @@
+//
+//  recommendViewController.h
+//  Foodys
+//
+//  Created by Matt Brax on 4/17/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface recommendViewController : UIViewController
+
+@end
---
+++ RecommendViewController.h
@@ -0,0 +1,14 @@
+//
+//  RecommendViewController.h
+//  Foodys
+//
+//  Created by Matt Brax on 4/17/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecommendViewController : UIViewController
+
+
+@end
