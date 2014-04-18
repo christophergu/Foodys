4ffe0f37b2c2036c3a70b19f729f36ee2b2b2fba
Christopher Gu <christopher.gu@gmail.com>
2014-04-18 13:39:50 -0500

    working on ratings

--- Foodys/ShareViewController.m
+++ Foodys/ShareViewController.m
@@ -17,12 +17,15 @@
 @property BOOL yesNoBool;
 @property (strong, nonatomic) IBOutlet UILabel *sliderScoreLabel;
 @property (strong, nonatomic) IBOutlet UISlider *mySlider;
+@property int sliderIntValue;
 @property (strong, nonatomic) IBOutlet UILabel *dateLabel;
 @property (strong, nonatomic) IBOutlet UIButton *recommendToFriendsButton;
 @property (strong, nonatomic) IBOutlet UIView *chooseFriendToWriteView;
 @property (strong, nonatomic) IBOutlet UIButton *chooseFriendsToWriteButton;
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *chooseFriendsDoneButton;
 @property (strong, nonatomic) IBOutlet UITableView *myTableView;
+@property (strong, nonatomic) IBOutlet UILabel *cumulativeRestaurantRatingLabel;
+@property (strong, nonatomic) IBOutlet UITextView *cumulativeRestaurantRatingsTextView;
 
 @property (strong, nonatomic) PFUser *currentUser;
 
@@ -34,19 +37,33 @@
 {
     [super viewDidLoad];
 
+    [self setUpBasicElementsForThisView];
+    self.currentUser = [PFUser currentUser];
+    self.yesNoBool = 0;
+    
+    [self cumulativeRestaurantRatingsLabelSetUp];
+    
+    // hiding the choose friends button
+    self.chooseFriendsDoneButton.tintColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0];
+    self.chooseFriendsDoneButton.enabled = NO;
+    
+//    int ya =[@"100" integerValue];
+//    
+//    NSLog(@"%d",ya);
+}
+
+-(void)setUpBasicElementsForThisView
+{
     self.myTextView.layer.cornerRadius=8.0f;
     self.myTextView.layer.masksToBounds=YES;
     self.myTextView.layer.borderColor=[[[UIColor grayColor] colorWithAlphaComponent:0.2] CGColor];
     self.myTextView.layer.borderWidth= 1.0f;
-
-    self.currentUser = [PFUser currentUser];
-    self.yesNoBool = 0;
     
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"yyyy-MM-dd"];
     NSDate *todayDate = [NSDate date];
     NSString *todayString = [dateFormat stringFromDate:todayDate];
-
+    
     self.dateLabel.text = [NSString stringWithFormat:@"%@",todayString];
     if (self.chosenRestaurantDictionary)
     {
@@ -54,10 +71,14 @@
     };
     
     [self.recommendToFriendsButton.titleLabel setTextAlignment: NSTextAlignmentCenter];
-    
-    // hiding the choose friends button
-    self.chooseFriendsDoneButton.tintColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0];
-    self.chooseFriendsDoneButton.enabled = NO;
+}
+
+-(void)cumulativeRestaurantRatingsLabelSetUp
+{
+    if ([self.cumulativeRestaurantRatingLabel.text isEqualToString:@""])
+    {
+        self.cumulativeRestaurantRatingsTextView.alpha = 0.0;
+    }
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -102,8 +123,8 @@
 
 - (IBAction)onMySliderChanged:(UISlider *)sender
 {
-    int discreteValue = roundl([sender value]);
-    self.sliderScoreLabel.text = [NSString stringWithFormat:@"%d", discreteValue];
+    self.sliderIntValue = roundl([sender value]);
+    self.sliderScoreLabel.text = [NSString stringWithFormat:@"%d", self.sliderIntValue];
 }
 
 - (IBAction)onChooseFriendsToWriteButtonPressed:(id)sender
@@ -143,14 +164,39 @@
     publicPost[@"author"] = self.currentUser[@"username"];
     publicPost[@"authorObjectId"] = self.currentUser.objectId;
     publicPost[@"date"] = [formatter dateFromString:self.dateLabel.text];
-    publicPost[@"title"] = self.myTextView.text;
-    publicPost[@"body"] = self.subjectTextField.text;
-    publicPost[@"rating"] = self.sliderScoreLabel.text;
+    publicPost[@"title"] = self.subjectTextField.text;
+    publicPost[@"body"] = self.myTextView.text;
+    int rating = [self.sliderScoreLabel.text integerValue];
+    publicPost[@"rating"] = @(rating);
     publicPost[@"wouldGoAgain"] = self.wouldGoAgainYesNoLabel.text;
     [publicPost saveInBackground];
     
     [self.currentUser addUniqueObject:publicPost forKey:@"postsMade"];
     [self.currentUser saveInBackground];
+    
+    
+    
+    
+    PFObject *reviewedRestaurant = [PFObject objectWithClassName:@"ReviewedRestaurant"];
+    reviewedRestaurant[@"name"] = self.subjectTextField.text;
+    if ([self.cumulativeRestaurantRatingLabel.text isEqualToString:@""])
+    {
+        NSLog(@"cumulative rating is nil");
+        if (reviewedRestaurant[@"ratingCounter"] == nil) {
+            reviewedRestaurant[@"ratingCounter"] = @(1);
+            reviewedRestaurant[@"rating"] = @(self.sliderIntValue);
+        }
+    }
+    else
+    {
+        int addingToTheRatingCounter = [reviewedRestaurant[@"ratingCounter"] intValue];
+        addingToTheRatingCounter += 1;
+        reviewedRestaurant[@"ratingCounter"] =  @(addingToTheRatingCounter);
+    }
+    int averagedRatingHolder = [self.cumulativeRestaurantRatingLabel.text integerValue];
+    reviewedRestaurant[@"rating"] = @((averagedRatingHolder + self.sliderIntValue)/[reviewedRestaurant[@"ratingCounter"] intValue]);
+    
+    [reviewedRestaurant saveInBackground];
 }
 
 @end
