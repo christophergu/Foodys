00d7716082ce3ae42d2f21770326a1661803127d
Christopher Gu <christopher.gu@gmail.com>
2014-04-22 11:10:27 -0500

    about to merge with matt's push origin master

---
+++ Foodys/NewsTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  NewsTableViewCell.h
+//  Foodys
+//
+//  Created by Matt Brax on 4/21/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) IBOutlet UILabel *recommended;
+@property (strong, nonatomic) IBOutlet UILabel *howMuch;
+@property (strong, nonatomic) IBOutlet UILabel *title;
+@property (strong, nonatomic) IBOutlet UITextView *content;
+@property (strong, nonatomic) IBOutlet UIImageView *avatarImageView;
+@end
