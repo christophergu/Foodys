3e9a3a65cf3d2cd941312746bf4fac555c4a2f3e
Christopher Gu <christopher.gu@gmail.com>
2014-04-28 22:26:22 -0500

    adding files that apparently didn't get added for the see recommendation view controller

--- Foodys/SeeRecommendationViewController.h
+++ Foodys/SeeRecommendationViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 @interface SeeRecommendationViewController : UIViewController
+@property (strong, nonatomic) NSDictionary *chosenRestaurantDictionary;
+@property (strong, nonatomic) PFObject *chosenRestaurantRecommendationObject;
 
 @end
--- Foodys/SeeRecommendationViewController.m
+++ Foodys/SeeRecommendationViewController.m
@@ -7,43 +7,62 @@
 //
 
 #import "SeeRecommendationViewController.h"
+#import "RestaurantViewController.h"
 
 @interface SeeRecommendationViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (strong, nonatomic) IBOutlet UILabel *restaurantRatingLabel;
+@property (strong, nonatomic) IBOutlet UILabel *reviewerNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *dateLabel;
+@property (strong, nonatomic) IBOutlet UITextView *recommendationBodyTextField;
+@property (strong, nonatomic) IBOutlet UIImageView *ratingFaceImageView;
+@property (strong, nonatomic) IBOutlet UILabel *restaurantTitleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *restaurantAddressLabel;
+
 
 @end
 
 @implementation SeeRecommendationViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    
+    self.avatarImageView.clipsToBounds = YES;
+    PFFile *userImageFile = self.chosenRestaurantRecommendationObject[@"avatar"];
+    [userImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+        if (!error) {
+            self.avatarImageView.image = [UIImage imageWithData:imageData];
+        }
+    }];
+    self.restaurantRatingLabel.text = [NSString stringWithFormat:@"%@%%",self.chosenRestaurantRecommendationObject[@"rating"]];
+    self.reviewerNameLabel.text = self.chosenRestaurantRecommendationObject[@"author"];
+    self.restaurantTitleLabel.text = self.chosenRestaurantRecommendationObject[@"restaurantDictionary"][@"name"];
+    self.restaurantAddressLabel.text = self.chosenRestaurantRecommendationObject[@"restaurantDictionary"][@"street_address"];
+    self.recommendationBodyTextField.text = self.chosenRestaurantRecommendationObject[@"body"];
+    
+    if ([self.chosenRestaurantRecommendationObject[@"rating"] intValue] > 74)
+    {
+        self.ratingFaceImageView.image = [UIImage imageNamed:@"smiley_icon"];
+    }
+    else if ([self.chosenRestaurantRecommendationObject[@"rating"] intValue] < 26)
+    {
+        self.ratingFaceImageView.image = [UIImage imageNamed:@"frowny_icon"];
+    }
+    
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"yyyy-MM-dd"];
+    NSDate *postDate = self.chosenRestaurantRecommendationObject[@"date"];
+    NSString *postString = [dateFormat stringFromDate:postDate];
+    
+    self.dateLabel.text = [NSString stringWithFormat:@"%@",postString];
 }
 
-- (void)didReceiveMemoryWarning
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    RestaurantViewController *rvc = segue.destinationViewController;
+    rvc.chosenRestaurantDictionary = self.chosenRestaurantRecommendationObject[@"restaurantDictionary"];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
